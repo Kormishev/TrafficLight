2eb44d23929cf7a73864a4353fb3603a5cec44b4
kormishev <kormishev@bk.ru>
2023-07-23 16:10:56 +0300

    Lights became round

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -1,9 +1,16 @@
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    @IBOutlet private var redLightView: UIView!
+    @IBOutlet private var yellowLightView: UIView!
+    @IBOutlet private var greenLightView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
 
     }
 
